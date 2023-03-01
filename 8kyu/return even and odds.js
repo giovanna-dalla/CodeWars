2cbfb593f3bec0c14028ec085dc292ce2a3d9aa7
Giovanna Dalla <giovannadalla2023@gmail.com>
2023-03-01 20:24:45 +1100

    Add files via upload

---
+++ 8kyu/return even and odds.js
@@ -0,0 +1,14 @@
+let array = [1,2,3,4,5,6,7,8,9]
+
+function evenAndOdds(array) { 
+    let even=[]; let odd=[];
+    for (let i =0; i<array.legnth; i++){
+        if (array[i]%2==0){
+            even.push(array[i])
+        }else{
+            odd.push(array[i])
+        }
+    }
+    console.log([even,odd])
+}
+
