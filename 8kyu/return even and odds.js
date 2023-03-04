0d9df4eb0224b9b499208e79e496777262fc8573
Giovanna Dalla <giovannadalla2023@gmail.com>
2023-03-04 13:25:13 +1100

    Update return even and odds.js

--- 8kyu/return even and odds.js
+++ 8kyu/return even and odds.js
@@ -2,8 +2,8 @@ let array = [1,2,3,4,5,6,7,8,9]
 
 function evenAndOdds(array) { 
     let even=[]; let odd=[];
-    for (let i =0; i<array.legnth; i++){
-        if (array[i]%2==0){
+    for (let i =0; i<array.length; i++){
+        if (array[i]%2===0){
             even.push(array[i])
         }else{
             odd.push(array[i])
