c73ef4442729f9cd7fcddcac6ca8708a3456d817
Giovanna Dalla <giovannadalla2023@gmail.com>
2023-03-19 12:05:59 +1100

    Add files via upload

---
+++ 8kyu/Average and round down.js
@@ -0,0 +1,20 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+    let answer = marks.reduce((acc,c)=>acc+c,0)/marks.length
+    return Math.floor(answer)
+  
+  }
+
+//   OR
+
+//   function getAverage(marks){
+//     return Math.floor(marks.reduce((acc,c)=>acc + c,)/marks.length)
+//   }
+
+
+  
